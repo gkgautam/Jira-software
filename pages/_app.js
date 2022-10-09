a51b38bb09e72573ccb616699ab055d053a93f31
Govind <govind@publicissapient.com>
2022-10-09 17:24:14 +0530

    linting

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,11 @@
 import Head from "next/head";
 import Script from "next/script";
 import Layout from "../components/Layout";
-// import "../css/customcss.css";
 function MyApp({ Component, pageProps }) {
 return (
 <>
 <Head>
-// Responsive meta tag
 <meta name="viewport" content="width=device-width, initial-scale=1" />
-//  bootstrap CDN
 <link
 href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
 integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
