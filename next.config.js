e791f66484ef44936be97857f99409b6fc7dd1f1
Govind <=>
2023-02-14 02:19:22 +0530

    env

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    mongodb_uri: "mongodb://root:jirasoftwaredb@ac-vuygd2n-shard-00-00.mbqqvaa.mongodb.net:27017,ac-vuygd2n-shard-00-01.mbqqvaa.mongodb.net:27017,ac-vuygd2n-shard-00-02.mbqqvaa.mongodb.net:27017/jirasoftware?ssl=true&replicaSet=atlas-bkre3u-shard-0&authSource=admin&retryWrites=true&w=majority",
+    mongodb_uri: process.env.MONGO_DB_URI,
   },
   swcMinify: true,
   sassOptions: {
