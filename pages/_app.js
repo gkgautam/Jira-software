dd05135293c77c7e501f55630d7ea32750084a75
Govind <=>
2023-02-15 22:30:05 +0530

    css login page

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Script from "next/script";
 import Layout from "../components/Layout";
 import { SessionProvider } from "next-auth/react";
+import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
     return (
         <>
