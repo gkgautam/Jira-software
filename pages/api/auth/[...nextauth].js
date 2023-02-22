ece1e8f6f14f96d9abc37607e6bfeb66d14d5888
Govind <=>
2023-02-22 21:01:34 +0530

    login through next AUth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,36 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import connectDB from "../../../middleware/database";
+import User from "../../../model/user";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { verify } from "argon2";
 export default NextAuth({
     secret:process.env.NEXTAUTH_SECRET,
     providers: [
         GoogleProvider({
           clientId: process.env.GOOGLE_CLIENT_ID,
           clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        }),
+        CredentialsProvider({
+          name:"Credentials",
+          async authorize(credentials,req){
+            try {
+              connectDB();
+              const isUserExist = await User.findOne({email:credentials.email});
+              if(!isUserExist){
+                throw new Error('User does not exist!');
+              }
+              else{
+                const checkPassword = await verify(isUserExist.password,credentials.password);
+                if(!checkPassword || isUserExist.email!==credentials.email){
+                  throw new Error('Invalid credentials!');
+                }
+                return isUserExist;
+              }
+            } catch (error) {
+              throw new Error(error);
+            }
+          }
         })
       ]
 });
