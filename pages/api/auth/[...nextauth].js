99df448ca2241012d84b7bfd9ae22f22efb067f7
Govind <=>
2023-02-25 22:28:12 +0530

    redploy final

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,35 +5,33 @@ import User from "../../../model/user";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { verify } from "argon2";
 export default NextAuth({
-    secret:process.env.NEXTAUTH_SECRET,
-    providers: [
-        GoogleProvider({
-          clientId: process.env.GOOGLE_CLIENT_ID,
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET
-        }),
-        CredentialsProvider({
-          name:"Credentials",
-          async authorize(credentials,req){
-            // connectDB().catch(err=>{err:"connection failed!"});
-            // try {
-              connectDB();
-              const isUserExist = await User.findOne({email:credentials.email});
-              console.log('usercheck',isUserExist);
-              if(!isUserExist){
-                throw new Error('User does not exist!');
-              }
-              else{
-                const checkPassword = await verify(isUserExist.password,credentials.password);
-                if(!checkPassword || isUserExist.email!==credentials.email){
-                  throw new Error('Invalid credentials!');
-                }
-                return isUserExist;
-              }
-            // } catch (error) {
-            //   console.log('usererr',error);
-            //   throw new Error(error);
-            // }
-          }
-        })
-      ]
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      async authorize(credentials, req) {
+        // connectDB().catch(err=>{err:"connection failed!"});
+        // try {
+        connectDB();
+        const isUserExist = await User.findOne({ email: credentials.email });
+        // console.log('usercheck', isUserExist);
+        if (!isUserExist) {
+          throw new Error('User does not exist!');
+        }
+        const checkPassword = await verify(isUserExist.password, credentials.password);
+        if (!checkPassword || isUserExist.email !== credentials.email) {
+          throw new Error('Invalid credentials!');
+        }
+        return isUserExist;
+        // } catch (error) {
+        //   console.log('usererr',error);
+        //   throw new Error(error);
+        // }
+      }
+    })
+  ]
 });
