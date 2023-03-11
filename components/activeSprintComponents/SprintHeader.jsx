3389c9c033f7d208f90d0a8e8df254aeb6c0220c
Govind <=>
2023-03-11 16:55:24 +0530

    Tailwind version css

--- components/activeSprintComponents/SprintHeader.jsx
+++ components/activeSprintComponents/SprintHeader.jsx
@@ -39,3 +39,48 @@ function SprintHeader() {
 }
 
 export default SprintHeader
+
+
+//below code of tailwaind
+
+// import React from 'react'
+// import { FaRegSun, FaRegClock } from "react-icons/fa";
+
+// function SprintHeader() {
+//   return (
+//     <>
+//       <div className='sprint-Header flex justify-between'>
+//         <div className='sprint-Title'>
+//           <h5 className=''>Web App PI4s4 </h5>
+//         </div>
+//         <div className='header-action flex justify-center items-center' style={{ "gap": "10px" }}>
+//           <div className='menu-button'>
+//             <svg role="presentation" width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.3789 4.62256C9.99914 4.62173 9.65331 4.84087 9.49192 5.18458L6.11523 12.3762C5.97308 12.679 5.9956 13.0334 6.17494 13.3157C6.35428 13.598 6.66553 13.7691 7 13.7691H8.79182L6.40251 18.1304C6.17135 18.5523 6.28163 19.0798 6.66249 19.3738C7.04335 19.6677 7.58152 19.6408 7.93117 19.3103L16.8269 10.902C17.1178 10.6269 17.2117 10.2023 17.0637 9.83032C16.9157 9.45832 16.5558 9.21418 16.1555 9.21418H14.547L17.0353 6.24031C17.2787 5.94947 17.3319 5.5441 17.1717 5.20034C17.0116 4.85658 16.6671 4.63646 16.2879 4.63562L10.3789 4.62256Z" stroke="currentColor" strokeWidth="1.95489" strokeLinejoin="round"></path></svg>
+//           </div>
+//           <div className='favourite'>
+//             <FaRegSun />
+//           </div>
+//           <div className='time-detail flex' style={{ "gap": "5px", "alignSelf": "center" }}>
+//             <span><FaRegClock /></span>
+//             <span className='mt-1' style={{ "fontSize": "14px", "color": "reg(66,82,110)", "marginTop": "4px" }}>2 days remaining</span>
+//           </div>
+//           <div className='complete-btn'>
+//             {/* <span style={{ "fontSize": "14px", "color": "reg(66,82,110)", "backgroundColor": "rgb(235,236,240)", "padding": "3px", "borderRadius": "2px" }}>complete sprint</span> */}
+//             <div className='custom-pirmary-btn' style={{"fontSize":"14px","backgroundColor": "rgba(9,30,66,0.04)", "padding": "1px 6px", "borderRadius": "2px","lineHeight":"24px","color":"#42526E"}}>
+//                             complete Sprint
+//                         </div>
+//           </div>
+//           <div className='share'>
+//             <svg width="20" height="20" viewBox="0 0 24 24" role="presentation"><g fill="currentColor" fillRule="evenodd"><path d="M6 15a3 3 0 100-6 3 3 0 000 6zm0-2a1 1 0 110-2 1 1 0 010 2zm12-4a3 3 0 100-6 3 3 0 000 6zm0-2a1 1 0 110-2 1 1 0 010 2zm0 14a3 3 0 100-6 3 3 0 000 6zm0-2a1 1 0 110-2 1 1 0 010 2z" fillRule="nonzero"></path><path d="M7 13.562l8.66 5 1-1.732-8.66-5z"></path><path d="M7 10.83l1 1.732 8.66-5-1-1.732z"></path></g></svg>
+//           </div>
+//           {/* <div>...</div> */}
+//           <div className='custom-pirmary-btn' style={{"fontSize":"24px","fontWeight":"500","backgroundColor": "rgba(9,30,66,0.04)", "padding": "1px 6px", "borderRadius": "2px","lineHeight":"16px","color":"#42526E","paddingBottom":"6px"}}>
+//                             <span style={{"verticalAlign":"super"}}>...</span>
+//                         </div>
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
+
+// export default SprintHeader
--- pages/viewissue/[IssueNo].js
+++ pages/viewissue/[IssueNo].js
@@ -284,7 +284,8 @@ function IssueNo({data}) {
                                     <TicketStatusDropDown updateTicketChanges={updateTicketChanges} ticketStatus={ticketStatus} setTicketStatus={setTicketStatus} /> 
 
                                 {/* </button> */}
-                                <div className='webapp-planning d-flex align-items-baseline'>
+                                {
+                                    ticketStatus==='done' ?  <div className='webapp-planning d-flex align-items-baseline'>
                                     <div className='dropdown-icon'>
                                         <svg width="20" height="20" viewBox="0 0 24 24" role="presentation"><path d="M6.735 12.322a1 1 0 00-1.47 1.356l3.612 3.919c.537.526 1.337.526 1.834.03l.364-.359a2335.638 2335.638 0 003.939-3.883l.04-.04a492.598 492.598 0 003.658-3.643 1 1 0 00-1.424-1.404 518.42 518.42 0 01-3.64 3.625l-.04.04a2049.114 2049.114 0 01-3.775 3.722l-3.098-3.363z" fill="rgb(0,102,68)"></path></svg>
                                     </div>
@@ -294,7 +295,9 @@ function IssueNo({data}) {
                                         {/* <p className='m-0 p-0' style={{ "fontSize": "12px", "fontWeight": "400", "color": "rgb(66,82,110)" }}>Board</p> */}
                                     </div>
 
-                                </div>
+                                </div> :null
+                                }
+                               
                             </div>
                             <div className='issue-user-actions mt-2' style={{ "border": "1px solid #EBECF0", "borderRadius": "3px" }}>
                                 <div className='action-heading d-flex justify-content-between px-2' style={{ "borderBottom": "1px solid #EBECF0" }}>
